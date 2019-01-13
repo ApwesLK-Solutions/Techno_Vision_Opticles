9725996220dec99fdc866fc70719ee9a5a153e02
Chamod Priyamal <chamod.priyamal@apweslk.com>
2019-01-13 15:19:08 +0530

    Begin to Draw UI

--- TechnoVisionOptical/TechnoVisionOptical/Program.cs
+++ TechnoVisionOptical/TechnoVisionOptical/Program.cs
@@ -16,7 +16,7 @@ namespace TechnoVisionOptical
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
