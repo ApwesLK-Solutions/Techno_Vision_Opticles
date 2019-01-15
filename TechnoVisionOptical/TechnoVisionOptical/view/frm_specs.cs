ac133c6df114b9c341a1854570613411990d4bb0
Nadishka Jayashan <nadishka.jayashan@gmail.com>
2019-01-15 18:54:47 +0530

    spectacles form updated

--- TechnoVisionOptical/TechnoVisionOptical/view/frm_specs.cs
+++ TechnoVisionOptical/TechnoVisionOptical/view/frm_specs.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TechnoVisionOptical.model;
 
 namespace TechnoVisionOptical.view
 {
@@ -15,6 +16,16 @@ namespace TechnoVisionOptical.view
         public frm_specs()
         {
             InitializeComponent();
+        }
+
+        private void txt_order_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            NewSpectaclesInvoice.FillFormOne(txt_order.Text, txt_orderdate.Value.ToString("YY-mm-DD"), txt_duedate.Value.ToString("YY-mm-DD"), txt_phone.Text, txt_name.Text, txt_address.Text, txt_email.Text, int.Parse(txt_age.Text), txt_proffession.Text, txt_eyeWear.Text, txt_lenses.Text, Double.Parse(txt_total.Text), cmb_pay_method.Text, cmb_pay_plan.Text, cmb_testedby.Text, Double.Parse(txt_advance.Text), Double.Parse(txt_discount.Text), Double.Parse(txt_balance.Text);
         }
     }
 }
--- TechnoVisionOptical/TechnoVisionOptical/view/frm_specs2.cs
+++ TechnoVisionOptical/TechnoVisionOptical/view/frm_specs2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TechnoVisionOptical.model;
 
 namespace TechnoVisionOptical.view
 {
@@ -20,6 +21,16 @@ namespace TechnoVisionOptical.view
         private void frm_specs2_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void metroTextBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            NewSpectaclesInvoice.FillFormTwo(txt_dist_sph_right.Text, txt_dist_cyl_right.Text, txt_dist_axis_right.Text, txt_dist_sph_left.Text, txt_dist_cyl_left.Text, txt_dist_axis_left.Text, txt_add_sph_right.Text, txt_add_cyl_right.Text, txt_add_axis_right.Text, txt_add_sph_left.Text, txt_add_cyl_left.Text, txt_add_axis_left.Text, txt_lense_type.Text, cmb_frame.Text, txt_PD.Text, txt_seg_height.Text, txt_specialInc.Text, txt_remarks.Text);
         }
     }
 }
